e7a3a0c6475ebcd755fad8f4df9faf1ffc592af3
wei chen <weichen9083@gmail.com>
2021-06-22 03:53:03 +0000

    Automatically backed up by Learn

--- db/migrate/20210622035123_create_recipes.rb
+++ db/migrate/20210622035123_create_recipes.rb
@@ -1,5 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
-    create_table :recipe 
+    create_table :recipes do |t|
+      t.string :name 
+      t.string :ingredients 
+      t.time 
+    end 
   end
 end
